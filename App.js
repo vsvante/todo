930661c6977d7df9f84015d1c05b4a1ebfeaacc8
Svante Vähäjylkkä <svante.vahajylkka@gmail.com>
2023-10-05 18:42:16 +0300

    todo

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, SafeAreaView, TextInput } from 'react-native';
+import Constants from 'expo-constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const STORAGE_KEY = '@todos'
 
 export default function App() {
+  const [newTodo,setNewTodo] = useState('')
+  const [todos, setTodos] = useState(
+    [{key: '0', description: 'Testing'}]
+  )
+
+  const storeData = async(value) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem(STORAGE_KEY, jsonValue)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  
+  const getData = async() => {
+    try {
+      return AsyncStorage.getItem(STORAGE_KEY)
+      .then(req => JSON.parse(req))
+      .then(json => {
+        if (json === null) {
+          json = []
+        }
+        setTodos(json)
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const addTodo = () => {
+    const newKey = String(todos.length)
+    const object = {key: newKey, description: newTodo}
+    const newTodos = [...todos, object]
+    storeData(newTodos)
+    setTodos(newTodos)
+    setNewTodo('')
+  }
+
+  
+useEffect(() => {
+  //AsyncStorage.clear()
+  getData()
+}, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.heading}>Todos</Text>
+      <TextInput
+        style={{ ...styles.input, borderColor: '#FAFAFA' }}
+        placeholder='Enter new todo...'
+        value={newTodo}
+        onChangeText={text => setNewTodo(text)}
+        returnKeyType='done'
+        onSubmitEditing={() => addTodo()}
+      />
+      <FlatList
+        style={styles.list}
+        data={todos}
+        extraData={todos}
+        renderItem={({item}) =>
+          <Text>{item.description}</Text>
+        }
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Constants.statusBarHeight,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  heading: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 8,
+    marginBottom: 8,
+  },
+  input: {
+    backgroundColor: '#F0F0F0',
+    borderColor: 'FAFAFA',
+    height: 40,
+    margin: 8,
+  },
+  list: {
+    margin: 8
+  },
+  row: {
+    height: 30,
+  }
 });
